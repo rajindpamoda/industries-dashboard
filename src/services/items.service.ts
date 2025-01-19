1ea1fdf269336b10d055d78f7b451d1095ba183b
rajind <pamoda1993@gmail.com>
2025-01-19 18:35:23 +0000

    Refactor return type in get() method of items service.

--- src/services/items.service.ts
+++ src/services/items.service.ts
@@ -1,7 +1,11 @@
 import BaseService from "./base.service.ts";
 import {IndustryItemsResponse} from "../shared/types/shared.types.ts";
 
-function get(): Promise<{ data: IndustryItemsResponse }> {
+type ItemServiceResponse = {
+    data: IndustryItemsResponse
+}
+
+function get(): Promise<ItemServiceResponse> {
     return BaseService.get("data", {}, {"Accept-Encoding":"gzip, compress, br"});
 }
 
